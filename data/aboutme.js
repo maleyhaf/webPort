f9d09c2519c75d80dc050d625638e71f52af65e3
maleyha <maleyha@uoguelph.ca>
2025-12-22 00:05:34 -0500

    tweaks

--- data/aboutme.js
+++ data/aboutme.js
@@ -9,6 +9,11 @@ const aboutSlides = [
       text: "As a forth-year Software Engineering student, I’m especially drawn to backend development and complex systems. I enjoy breaking down complicated problems, working through edge cases, and building efficient, reliable solutions where small details can have a big impact.",
       image: "/assets/about2.jpg",
    },
+   {
+      title: "Always Learning & Growing",
+      text: "I’m always open to learning new technologies and expanding my technical skill set. The fast pace of the tech world excites me — each new language, tool, or concept feels like another step toward becoming a stronger, more adaptable developer who values growth, creativity, and collaboration.",
+      image: "/assets/about3.JPG",
+   },
 
    {
       title: "What Motivates Me",
@@ -20,13 +25,13 @@ const aboutSlides = [
    {
       title: "Creative Hobbies",
       text: "Outside of tech, I find joy in creative hobbies like crocheting, arts and crafts, and journaling. I especially love crocheting plushies, which often become gifts for my friends and family.",
-      image: "/assets/about7.jpg",
+      image: "/assets/about7.JPG",
    },
 
    {
       title: "Relaxation & Curiosity",
       text: "I enjoy unwinding with TV shows and movies, especially those about Greek mythology, and I love learning about psychology and human behavior. Plants, animals, and journaling also hold a special place in my heart.",
-      image: "/assets/about9.jpg",
+      image: "/assets/about9.JPG",
    },
 ];
 
