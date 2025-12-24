802e83875026d23e68929216a3c8e8fb353de9b3
maleyha <maleyha@uoguelph.ca>
2025-12-24 10:23:42 -0500

    tweak

--- components/SkillIcon.jsx
+++ components/SkillIcon.jsx
@@ -8,7 +8,7 @@ export default function SkillIcon({ icon, label, delay = 0 }) {
       <i
         className={`${icon} text-5xl text-accent-light transition-transform duration-200 group-hover:scale-110`}
       />
-      <span className="mt-2 text-sm opacity-0 group-hover:opacity-100 transition">
+      <span className="mt-2 text-sm text-cream opacity-0 group-hover:opacity-100 transition">
         {label}
       </span>
     </div>
